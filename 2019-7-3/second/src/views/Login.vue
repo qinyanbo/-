9a9d424fbe578bce3e384c2cada83fab27cc426f
qinyanbo <aliboit@163.com>
2019-07-03 16:48:20 +0800

    Add second

---
+++ 2019-7-3/second/src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="login-wrapper">
+    <div class="login-form"></div>
+    <div class="login-logo"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.login-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: linear-gradient(to bottom right, #318fce 0, #48e7ef);
+}
+.login-wrapper .login-logo {
+    position: absolute;
+    top: 10px;
+    left: 30px;
+    width: 250px;
+    height: 75px;
+    background: url(../assets/logo.png) no-repeat 50%;
+    background-size: cover;
+}
+</style>
