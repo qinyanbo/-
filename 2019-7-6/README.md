c201eb2d2be70f0b642a90ae89875393e639c4bf
qinyanbo <aliboit@163.com>
2019-07-06 14:16:25 +0800

    Add codes of 7.6

---
+++ 2019-7-6/README.md
@@ -0,0 +1,4 @@
+- [axios npm官网](https://www.npmjs.com/package/axios)
+
+
+- [Axios 各种请求方式传递参数格式](https://www.jianshu.com/p/53deecb09077)
---
+++ 2019-7-6/second/src/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// axios.defaults.baseURL = 'http://47.106.101.129:9999/';
+
+axios.defaults.baseURL = 'http://www.brotherbo.cn:9999/';
+
+
+export default axios;
